d2e59d8316c8192f695b17bf1eaca104686a1812
YinggangDong <believerD@aliyun.com>
2020-11-09 17:50:46 +0800

    style:修改格式

--- com/snbc/java/singleEncryptionAlgorithm/Base64/JDKBase64.java
+++ com/snbc/java/singleEncryptionAlgorithm/Base64/JDKBase64.java
@@ -7,15 +7,16 @@ import java.io.IOException;
 
 /**
  * BASE64算法使用：1.jdk包下的Base64的使用(这里只演示jdk包下的Base64的使用)
- *               2.Commons codec包的Base64的使用
- *               3.Bouncy Castle包的Base64的使用
+ * 2.Commons codec包的Base64的使用
+ * 3.Bouncy Castle包的Base64的使用
+ *
  * @author Lee Xiang
  * @date 2020/10/21 19:06
  **/
 public class JDKBase64 {
     private static String str = "snbc.security.base64";
 
-    public static void jdkBase64(){
+    public static void jdkBase64() {
         try {
             //编码
             BASE64Encoder encoder = new BASE64Encoder();
